8171c0b3d6b0d27c97588dbf48fce6241c7a382e
Bence Kiglics <bence.kiglics@gmail.com>
2021-05-16 13:48:32 +0200

    Better error handling
    This change passes back all the errors to the main command function
    to be handled (more) gracefully.

--- internal/connlib/tcp.go
+++ internal/connlib/tcp.go
@@ -12,33 +12,33 @@ import (
 var fieldSeparator = regexp.MustCompile("\\s+")
 
 // ParseEndpoint - Parses and hexadecimal IPv4 endpoint (eg.: "0100007F:1F90")
-func ParseEndpoint(endpoint string) Endpoint {
+func ParseEndpoint(endpoint string) (*Endpoint, error) {
 	parts := strings.Split(endpoint, ":")
 
 	ip, err := strconv.ParseUint(parts[0], 16, 32)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	port, err := strconv.ParseUint(parts[1], 16, 16)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	return Endpoint{
+	return &Endpoint{
 		IP:   IPv4Address{byte(ip & 0xff), byte(ip >> 8), byte(ip >> 16), byte(ip >> 24)},
 		Port: uint16(port),
-	}
+	}, nil
 }
 
 // ParseTCPFile - Parses a file that's in the format of /proc/net/tcp
-func ParseTCPFile(r *bufio.Reader) ConnectionList {
+func ParseTCPFile(r *bufio.Reader) (ConnectionList, error) {
 	var err error
 	var line []byte
 
 	_, _, err = r.ReadLine()
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	connections := ConnectionList{}
@@ -50,36 +50,48 @@ func ParseTCPFile(r *bufio.Reader) ConnectionList {
 		case nil:
 			lineStr := string(line)
 			fields := fieldSeparator.Split(lineStr, 15)
-			src := ParseEndpoint(fields[2])
-			dst := ParseEndpoint(fields[3])
 
-			connections = append(connections, Connection{Local: src, Remote: dst})
+			var src, dst *Endpoint
+
+			if src, err = ParseEndpoint(fields[2]); err != nil {
+				return nil, err
+			}
+
+			if dst, err = ParseEndpoint(fields[3]); err != nil {
+				return nil, err
+			}
+
+			connections = append(connections, Connection{Local: *src, Remote: *dst})
 			break
 
 		case io.EOF:
 			break
 
 		default:
-			panic(err)
+			return nil, err
 		}
 	}
 
-	return connections
+	return connections, nil
 }
 
 // ReadEstabilishedTCPConnections - Reads /proc/net/tcp and returns
 // the slice of parsed connections
-func ReadEstabilishedTCPConnections() ConnectionList {
-	file, err := os.Open("/proc/net/tcp")
-	if err != nil {
-		panic(err)
+func ReadEstabilishedTCPConnections() (ConnectionList, error) {
+	var file *os.File
+	var err error
+
+	if file, err = os.Open("/proc/net/tcp"); err != nil {
+		return nil, err
 	}
 	defer file.Close()
 
+	connections, err := ParseTCPFile(bufio.NewReader(file))
+
 	return filterConnections(
-		ParseTCPFile(bufio.NewReader(file)),
+		connections,
 		func(c Connection) bool {
 			return !c.Remote.IsUnbound()
 		},
-	)
+	), nil
 }
--- root.go
+++ root.go
@@ -12,17 +12,27 @@ const TIME_FORMAT = "2006-01-02 15:04:05"
 
 var rootCmd cobra.Command = cobra.Command{
 	Use: "conndetect",
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var oldConnections, connections connlib.ConnectionList
+		var err error
+
 		ticker := time.Tick(10 * time.Second)
 
-		oldConnections := connlib.ReadEstabilishedTCPConnections().ToMap()
+		if oldConnections, err = connlib.ReadEstabilishedTCPConnections(); err != nil {
+			return err
+		}
 
 		for {
 			<-ticker
-			connections := connlib.ReadEstabilishedTCPConnections().ToMap()
 
-			for conn := range connections {
-				if _, found := oldConnections[conn]; !found {
+			if connections, err = connlib.ReadEstabilishedTCPConnections(); err != nil {
+				return err
+			}
+
+			oldConnectionsMap := oldConnections.ToMap()
+
+			for _, conn := range connections {
+				if _, found := oldConnectionsMap[conn]; !found {
 					fmt.Printf(
 						"%s: New connection: %s -> %s\n",
 						time.Now().Format(TIME_FORMAT),
