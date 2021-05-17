f90f013253b7e9dc198fb2f78da2c6a04da9d3ef
Bence Kiglics <bence.kiglics@gmail.com>
2021-05-17 19:12:36 +0200

    Add GitHub actions and a Makefile

--- test.sh
+++ test.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 ROOT=$(cd "$(dirname ${BASH_SOURCE[0]:-$0})"; pwd)
 
 cd "$ROOT"
-go test $(find . -name '*_test.go' | sed 's/[^/]\+$//' | uniq)
+
+go get -d .
+
+for path in $(find . -name '*_test.go' | sed 's/[^/]\+$//' | uniq); do
+    go test "$path"
+done
