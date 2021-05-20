59cd169100f5433196a2b0e150a3df8f2c9d8569
Bence Kiglics <bence.kiglics@gmail.com>
2021-05-20 10:52:18 +0200

    Replaced for loop with ./...

--- test.sh
+++ test.sh
@@ -7,7 +7,4 @@ ROOT=$(cd "$(dirname ${BASH_SOURCE[0]:-$0})"; pwd)
 cd "$ROOT"
 
 go get -d .
-
-for path in $(find . -name '*_test.go' | sed 's/[^/]\+$//' | uniq); do
-    go test "$path"
-done
+go test ./...
